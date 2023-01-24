db50b4f21dbff2373402ed301568a88194d96dbc
avengingtooth <esellin@gmail.com>
2023-01-24 11:52:42 +0100

    completed

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,100 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength){
+        this.health = health
+        this.strength = strength
+    }
+    
+    receiveDamage(damage){
+        this.health -= damage
+    }
+
+    attack(){
+        return this.strength
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+    constructor(name, health, strength){
+        super(health, strength)
+        this.name = name
+    }
+
+    attack(){
+        return this.strength
+    }
+
+    receiveDamage(damage){
+        super.receiveDamage(damage)
+        if (this.health > 0){
+            return `${this.name} has received ${damage} points of damage`
+        }
+        else{
+            return `${this.name} has died in act of combat`
+        }
+    }
+
+    battleCry(){
+        return "Odin Owns You All!"
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier {
+    constructor(health, strength){
+        super(health, strength)
+    }
+    receiveDamage(damage){
+        super.receiveDamage(damage)
+        if (this.health > 0){
+            return `A Saxon has received ${damage} points of damage`
+        }
+        else{
+            return `A Saxon has died in combat`
+        }
+    }
+}
 
 // War
-class War {}
+class War {
+    constructor(){
+        this.vikingArmy = []
+        this.saxonArmy = []
+    }
+
+    addViking(viking){
+        this.vikingArmy.push(viking)
+    }
+
+    addSaxon(saxon){
+        this.saxonArmy.push(saxon)
+    }
+
+    vikingAttack(){
+        let r = ''
+        this.saxonArmy.map(e => r = e.receiveDamage(this.vikingArmy[0].attack()))
+        this.saxonArmy = this.saxonArmy.filter(element => element.health > 0)
+        return r
+    }
+
+    saxonAttack(){
+        let r = ''
+        this.vikingArmy.map(e => {return r = e.receiveDamage(this.saxonArmy[0].attack())})
+        this.vikingArmy = this.vikingArmy.filter(element => element.health > 0)
+        return r
+    }
+
+    showStatus(){
+        if (!this.saxonArmy.length){
+            return 'Vikings have won the war of the century!'
+        }
+        else if (!this.vikingArmy.length){
+            return 'Saxons have fought for their lives and survived another day...'
+        }
+        else{
+            console.log(this.vikingArmy, 'viking')
+            return "Vikings and Saxons are still in the thick of battle."
+        }
+    }
+}
